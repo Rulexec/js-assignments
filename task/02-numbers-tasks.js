bcb5843a1ab082a278eb90fa4ee0df0f95700576
Alexander Ruliov <alexander.ruliov@gmail.com>
2016-04-10 13:04:59 +0300

    fix getAngleBetweenVectors

--- task/02-numbers-tasks.js
+++ task/02-numbers-tasks.js
@@ -115,7 +115,7 @@ function getLinearEquationRoot(a, b) {
  *   (0,1) (1,2)     => 0
  */
 function getAngleBetweenVectors(x1, y1, x2, y2) {
-  return Math.atan(y2 / x2) - Math.atan(y1 / x1);
+  return Math.abs(Math.atan(y2 / x2) - Math.atan(y1 / x1));
 }
 
 /**

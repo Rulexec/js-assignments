357c24f613f66041b3a0dbc117924fcfc8574c93
Alexander Ruliov <alexander.ruliov@gmail.com>
2016-04-17 03:32:13 -0400

    date tasks

--- task/03-date-tasks.js
+++ task/03-date-tasks.js
@@ -22,7 +22,7 @@
  *    'Sun, 17 May 1998 03:00:00 GMT+01' => Date()
  */
 function parseDataFromRfc2822(value) {
-   throw new Error('Not implemented');
+  return new Date(value);
 }
 
 /**
@@ -37,7 +37,7 @@ function parseDataFromRfc2822(value) {
  *    '2016-01-19T08:07:37Z' => Date()
  */
 function parseDataFromIso8601(value) {
-   throw new Error('Not implemented');
+  return new Date(value);
 }
 
 
@@ -56,7 +56,9 @@ function parseDataFromIso8601(value) {
  *    Date(2015,1,1)    => false
  */
 function isLeapYear(date) {
-   throw new Error('Not implemented');
+  let year = date.getFullYear();
+
+  return (year % 400 === 0) || (year % 100 !== 0 && year % 4 === 0);
 }
 
 
@@ -76,7 +78,7 @@ function isLeapYear(date) {
  *    Date(2000,1,1,10,0,0),  Date(2000,1,1,15,20,10,453)   => "05:20:10.453"
  */
 function timeSpanToString(startDate, endDate) {
-   throw new Error('Not implemented');
+  return new Date(endDate - startDate).toISOString().slice(11, -1);
 }
 
 
@@ -94,7 +96,19 @@ function timeSpanToString(startDate, endDate) {
  *    Date.UTC(2016,3,5,21, 0) => Math.PI/2
  */
 function angleBetweenClockHands(date) {
-    throw new Error('Not implemented');
+  let hours = date.getUTCHours(),
+      minutes = date.getUTCMinutes();
+
+  let a = minutes / 60,
+      b = (hours % 12 + a) / 12
+
+  let result = Math.abs(a - b);
+
+  if (result > 0.5) {
+    result = Math.min(a, b) + (1 - Math.max(a, b));
+  }
+
+  return result * 2 * Math.PI;
 }
 
 
